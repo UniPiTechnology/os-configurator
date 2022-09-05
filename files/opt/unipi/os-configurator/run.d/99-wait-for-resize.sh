feafa94da5cdd6672a85cf261438714d02540414
Miroslav Ondra <ondra@faster.cz>
2022-09-05 18:05:17 +0200

    Add waiting for some service before reboot

---
+++ files/opt/unipi/os-configurator/run.d/99-wait-for-resize.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+MAX_WAIT=10
+
+if systemctl -q is-enabled unipi-resize-partition.service 2>/dev/null; then
+    echo "Waiting for partition resize"
+    while systemctl -q is-enabled unipi-resize-partition.service; do
+        [ "${MAX_WAIT}" = "0" ] && break
+        MAX_WAIT=$(($MAX_WAIT-1))
+        sleep 1
+    done
+fi
+
+if systemctl -q is-enabled regenerate_ssh_host_keys.service 2>/dev/null; then
+    echo "Waiting for partition resize"
+    while systemctl -q is-enabled regenerate_ssh_host_keys.service; do
+        [ "${MAX_WAIT}" = "0" ] && break
+        MAX_WAIT=$(($MAX_WAIT-1))
+        sleep 1
+    done
+fi
+
+exit 0
